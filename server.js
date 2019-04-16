9e58ee3d2d45eafff9b004facef92dbcf211454f
leeroywking <leeroywking@gmail.com>
2019-04-15 21:19:22 -0700

    more steps completed

--- server.js
+++ server.js
@@ -4,3 +4,25 @@ require('dotenv').config();
 
 const express = require('express');
 
+const app = express();
+
+const PORT = process.env.PORT;
+
+app.use(express.static('./public'));
+
+app.get('/hello', (request, response) => {
+    response.status(200).send('<h1>Hello</h1>')
+});
+
+app.get('/data', (request, response) => {
+    let airplane = {
+        departure: Date.now(),
+        canFly: true, 
+        pilot: 'Drunk'
+    }
+    response.status(200).json(airplane);
+});
+
+app.use('*', (request, response) =>  response.send('Sorry that rouse does not exist dummy'));
+
+app.listen(PORT, () => console.log(`My ?fiftieth? Server is listening on port ${PORT}`));
