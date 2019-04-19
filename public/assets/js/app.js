bd30b8c33e8d89e635fed4cf098fa8cb004e6bff
leeroywking <leeroywking@gmail.com>
2019-04-18 20:11:32 -0700

    this is stupid and I don't understand why we are doing this

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,15 +1,28 @@
 
 
-githubDataPull = () => {
-    return $.get('https://api.github.com/users/leeroywking/repos', 'json')
-  };
+// githubDataPull = () => {
+//     return $.get('https://api.github.com/users/leeroywking/repos', 'json')
+//   };
 
-githubDataParse = () => {
-    return console.log(githubDataPull().responseJSON)
-}
+// githubDataParse = () => {
+//     return console.log(githubDataPull().responseJSON)
+// }
 
-githubDataParse();
+// githubDataParse();
 
 // $('#main > section.one')
-// .css('background-image','url(../../images/banner.jpg)')
-// .fadeOut(10000)
+// .css('background-image','url(../../images/pano.jpg)')
+
+
+const nextBackground = BGindex => {
+  let backgrounds = [
+    'url(../../images/pano.jpg)',
+    'url(../../images/banner.jpg)'
+  ]
+  $('#main > section.one').css('background-image', backgrounds[BGindex]);
+  setTimeout(nextBackground(1), 100000)
+}
+
+nextBackground(0);
+nextBackground(1);
+
